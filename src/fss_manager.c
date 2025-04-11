424079610326475648cfeab06d151731e5981da8
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2025-04-11 11:28:40 +0300

    function to create named pipes

---
+++ src/fss_manager.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+
+void create_named_pipes() {
+    // Remove existing pipes if they exist
+    unlink("fss_in");
+    unlink("fss_out");
+    
+    // Create new named pipes
+    mkfifo("fss_in", 0666);
+    mkfifo("fss_out", 0666);
+}
