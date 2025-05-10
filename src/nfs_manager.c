6752092dd4d8b405e4d3ba805936a7810acc37ad
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2025-05-10 16:36:59 +0300

    command line parsing

--- src/nfs_manager.c
+++ src/nfs_manager.c
@@ -32,6 +32,50 @@ int port_number = 0;
 int buffer_size = 0;
 
 int main(int argc, char *argv[]) {
-	
+	if (argc < 9) {
+        fprintf(stderr, "Usage: %s -l <logfile> -c <config_file> [-n <worker_limit>] -p <port_number> -b <bufferSize>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+	for (int i = 1; i < argc; i += 2) {
+        if (!strcmp(argv[i], "-l") && i + 1 < argc) {
+            strncpy(manager_logfile, argv[i + 1], sizeof(manager_logfile));
+        }
+		else if (!strcmp(argv[i], "-c") && i + 1 < argc) {
+            strncpy(config_file, argv[i + 1], sizeof(config_file));
+        }
+		else if (!strcmp(argv[i], "-n") && i + 1 < argc) {
+            worker_limit = atoi(argv[i + 1]);
+			if(worker_limit <= 0) {
+				fprintf(stderr, "Worker limit should be a positive integer\n");
+				exit(EXIT_FAILURE);
+			}
+        }
+		else if (!strcmp(argv[i], "-p") && i + 1 < argc) {
+            port_number = atoi(argv[i + 1]);
+			if(port_number <= 0) {
+				fprintf(stderr, "Port number should be a positive integer\n");
+				exit(EXIT_FAILURE);
+			}
+        }
+		else if (!strcmp(argv[i], "-b") && i + 1 < argc) {
+            buffer_size = atoi(argv[i + 1]);
+			if(buffer_size <= 0) {
+				fprintf(stderr, "Buffer size should be a positive integer\n");
+				exit(EXIT_FAILURE);
+			}
+        }
+		else {
+            fprintf(stderr, "Unknown or incomplete option %s\n", argv[i]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("manager_logfile: %s\n", manager_logfile);
+    printf("config_file: %s\n", config_file);
+    printf("worker_limit: %d\n", worker_limit);
+    printf("port_number: %d\n", port_number);
+    printf("buffer_size: %d\n", buffer_size);
+
 	return 0;
 }
